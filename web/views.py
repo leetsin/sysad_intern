fc94e1d14a66c949253e3cdeb0d7c825a04f8cbc
shiv <shiv07221202021@msijanakpuri.com>
2024-02-17 22:17:52 +0000

    Changed views

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Test Index Page")
+    return HttpResponse("Hi shiv")
 
 def about(request):
     return HttpResponse("Test About Page")
